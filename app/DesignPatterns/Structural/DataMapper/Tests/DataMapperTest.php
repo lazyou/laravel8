4fb8b2152cf2de25a24de4f2c8236f0dc574106a
lazyou <1394139539@qq.com>
2021-02-20 15:31:12 +0800

    设计模式：数据映射模式

---
+++ app/DesignPatterns/Structural/DataMapper/Tests/DataMapperTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\DesignPatterns\Structural\DataMapper\Tests;
+
+use InvalidArgumentException;
+use App\DesignPatterns\Structural\DataMapper\StorageAdapter;
+use App\DesignPatterns\Structural\DataMapper\User;
+use App\DesignPatterns\Structural\DataMapper\UserMapper;
+use PHPUnit\Framework\TestCase;
+
+class DataMapperTest extends TestCase
+{
+    public function testCanMapUserFromStorage()
+    {
+        $storage = new StorageAdapter([1 => ['username' => 'domnikl', 'email' => 'liebler.dominik@gmail.com']]);
+        $mapper = new UserMapper($storage);
+
+        $user = $mapper->findById(1);
+
+        $this->assertInstanceOf(User::class, $user);
+    }
+
+    public function testWillNotMapInvalidData()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $storage = new StorageAdapter([]);
+        $mapper = new UserMapper($storage);
+
+        $mapper->findById(1);
+    }
+}
