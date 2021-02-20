10abd63cede383447656aeae1bdbb03de54b2e0c
lazyou <1394139539@qq.com>
2021-02-20 09:38:48 +0800

    设计模式：简单工厂模式

---
+++ app/DesignPatterns/Creational/SimpleFactory/Tests/SimpleFactoryTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\DesignPatterns\Creational\SimpleFactory\Tests;
+
+use App\DesignPatterns\Creational\SimpleFactory\Bicycle;
+use App\DesignPatterns\Creational\SimpleFactory\SimpleFactory;
+use PHPUnit\Framework\TestCase;
+
+class SimpleFactoryTest extends TestCase
+{
+    public function testCanCreateBicycle()
+    {
+        $bicycle = (new SimpleFactory())->createBicycle();
+        $this->assertInstanceOf(Bicycle::class, $bicycle);
+    }
+}
