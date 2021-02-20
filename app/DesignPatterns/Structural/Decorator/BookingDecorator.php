9e0f7decc50233d7ef81919f186924d06df827f0
lazyou <1394139539@qq.com>
2021-02-20 16:17:39 +0800

    add comment

--- app/DesignPatterns/Structural/Decorator/BookingDecorator.php
+++ app/DesignPatterns/Structural/Decorator/BookingDecorator.php
@@ -2,6 +2,13 @@
 
 namespace App\DesignPatterns\Structural\Decorator;
 
+/**
+ * 装饰者必须实现渲染接口类 Booking 契约，这是该设计模式的关键点。
+ * 否则，这将不是一个装饰者而只是一个自欺欺人的包装。
+ *
+ * Class BookingDecorator
+ * @package App\DesignPatterns\Structural\Decorator
+ */
 abstract class BookingDecorator implements Booking
 {
     protected Booking $booking;
