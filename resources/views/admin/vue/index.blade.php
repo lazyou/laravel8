68fa9a8e7c0569aae2630a2e34bd6a40ef2b2891
lazyou <1394139539@qq.com>
2021-03-02 09:48:32 +0800

    vue: 优化

--- resources/views/admin/vue/index.blade.php
+++ resources/views/admin/vue/index.blade.php
@@ -1,20 +1,18 @@
 @extends('_layout.app')
 
 @section('content')
-    <div id="content">
-        <el-button @click="visible = true">Button</el-button>
-        <el-link href="/admin/vue">默认链接</el-link>
+    <el-button @click="visible = true">Button</el-button>
+    <el-link href="/admin/vue">默认链接</el-link>
 
-        <el-dialog :visible.sync="visible" title="Hello world">
-            <p>Try Element</p>
-        </el-dialog>
+    <el-dialog :visible.sync="visible" title="Hello world">
+        <p>Try Element</p>
+    </el-dialog>
 
-        <el-table :data="tableData"  v-loading="loading" :data="data" border fit highlight-current-row>
-            <el-table-column prop="date" label="日期" width="180"></el-table-column>
-            <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-            <el-table-column prop="address" label="地址"></el-table-column>
-        </el-table>
-    </div>
+    <el-table :data="tableData"  v-loading="loading" :data="data" border fit highlight-current-row>
+        <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
 @endsection
 
 @section('content-vue')
